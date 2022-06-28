4aa2662420b251b0287cfbffc5195371ee9b8dfa
mfarslanturk <arslantrkfatih@gmail.com>
2022-06-28 14:03:17 +0300

    utils imported

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
-const fs = require('fs');
+const fs = require("fs");
+const { sayMyName: printMyName } = require("./utils");
 
-fs.writeFileSync('notes.txt','This file was created by Node.js!');
+fs.writeFileSync("notes.txt", "This file was created by Node.js!");
 
-fs.appendFileSync('notes.txt',' Solved Challenge!');
+fs.appendFileSync("notes.txt", " Solved Challenge!");
+
+printMyName();
