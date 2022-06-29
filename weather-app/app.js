f4a046035314ab5626a034e7583ad2777b626546
mfarslanturk <arslantrkfatih@gmail.com>
2022-06-29 14:53:28 +0300

    forecast and geocode append with callback chain

--- weather-app/app.js
+++ weather-app/app.js
@@ -15,3 +15,24 @@ function process2(){
 }
 
 process1(process2);
+
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+}
