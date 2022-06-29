3ee43b5d22e5314a84f35f331731e5705fef2b68
mfarslanturk <arslantrkfatih@gmail.com>
2022-06-29 09:12:03 +0300

    crud operations created

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,3 +1,24 @@
-export default function getNotes() {
-  return 'Notes..'
+import fs from 'fs'
+import chalk from 'chalk'
+
+export const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+export const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    console.log('first')
+    // fs.writeFileSync('notes.json', dataJSON)
 }
