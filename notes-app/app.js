94c016efe5eb30f02e6fd31ca6bb4656c82faa38
mfarslanturk <arslantrkfatih@gmail.com>
2022-06-29 09:16:24 +0300

    yargs crud operations added

--- notes-app/app.js
+++ notes-app/app.js
@@ -42,3 +42,46 @@ yargs.command({
         notes.addNote(argv.title, argv.body)
     }
 })
+
+// remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
