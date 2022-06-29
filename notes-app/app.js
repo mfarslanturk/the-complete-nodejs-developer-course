34e588cbf511b77cf46e5d25606a6a4efbbc0b28
mfarslanturk <arslantrkfatih@gmail.com>
2022-06-29 08:45:31 +0300

    crud operations added from terminal

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,20 @@
-import fs from 'fs'
-import chalk from 'chalk'
-import getNotes from './notes.js'
+import fs from "fs";
+import chalk from "chalk";
+import getNotes from "./notes.js";
 
+const command = process.argv[2];
 
+switch (command) {
+  case 'add':
+    console.log(chalk.blue("Added!"));
+    break;
+  case "delete":
+    console.log(chalk.red("Deleted!"));
+    break;
+  case "update":
+    console.log(chalk.yellow("Updated!"));
+    break;
+  default:
+    console.log(chalk.green("Check your input!!"));
+    break;
+}
