b7d3f09f1941cb11f77438485fa9d9272e4f5fea
mfarslanturk <arslantrkfatih@gmail.com>
2022-06-29 14:11:31 +0300

    callback example

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,17 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+function process1(callback){
+    setTimeout(function () {
+        console.log("Process1")
+        callback();
+    },3000)
+}
 
+function process2(){
+    setTimeout(function () {
+        console.log("Process2")
+    },3000)
+}
+
+process1(process2);
