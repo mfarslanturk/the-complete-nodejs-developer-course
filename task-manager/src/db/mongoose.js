a18f94c5aa91162503bde2a8f082a8d0a56202e0
mfarslanturk <arslantrkfatih@gmail.com>
2022-07-05 15:04:19 +0300

    Section13 completed

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,10 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/task-manager-api');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/task-manager-api"),
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  };
 
 // ,{
 //     useNewUrlParser:true,
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,14 +26,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
+// const Task = require('./models/task')
+// const User = require('./models/user')
 
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
+// const main = async () => {
+    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
 
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
+    // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
+    // await user.populate('tasks').execPopulate()
+    // console.log(user.tasks)
+// }
 
-myFunction()
+// main()
